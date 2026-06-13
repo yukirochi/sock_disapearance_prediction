e6dfac25b73a59b5fc6fd917474f0385ad0e3923
yukirochi <justineabanilla81@gmail.com>
2026-06-13 21:06:54 +0800

    partial

---
+++ snowflake_info.py
@@ -0,0 +1,23 @@
+import snowflake.connector
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+
+conn = snowflake.connector.connect(
+    user=os.getenv("SNOWFLAKE_USER"),
+    password=os.getenv("SNOWFLAKE_PASSWORD"),
+    account=os.getenv("SNOWFLAKE_ACCOUNT"),
+    warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+    database=os.getenv("SNOWFLAKE_DATABASE"),
+    schema=os.getenv("SNOWFLAKE_SCHEMA")
+)
+
+
+curr = conn.cursor()
+
+sql = 'SELECT * FROM mart.MART_DATA'
+
+df = curr.execute(sql).fetch_pandas_all()
+
